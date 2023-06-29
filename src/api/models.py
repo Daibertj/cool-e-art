a409cd1a431f907b7f9f9effe867c3495bc382fd
Daibert Ramirez <126725296+Daibertj@users.noreply.github.com>
2023-06-29 23:16:10 +0000

    modelado tabla user

--- src/api/models.py
+++ src/api/models.py
@@ -4,9 +4,16 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), unique=False, nullable=False )
+    lastname = db.Column(db.String(30), unique=False, nullable=False )
+    username = db.Column(db.String(30), unique=False, nullable=True )
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    image = db.Column(db.String(255), unique=False, nullable=True )
+    create_at = db.Column(db.DateTime, nullable=False,
+                          default=db.func.current_timestamp())
+    updated_at = db.Column(db.DateTime, nullable=False, onupdate=db.func.current_timestamp(
+    ), default=db.func.current_timestamp())
 
     def __repr__(self):
         return f'<User {self.email}>'
@@ -15,5 +22,10 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
+            "name": self.name,
+            "lastname": self.lastname,
+            "username": self.username
             # do not serialize the password, its a security breach
         }
+
+class 
