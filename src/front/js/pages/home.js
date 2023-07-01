553f68200a95bbcb2deb99d8a41afe14f38c24cf
tuquepantin <tuque250@gmail.com>
2023-07-01 02:30:00 +0000

    added card component

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,26 +1,20 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import rigoImageUrl from "../../img/rigo-baby.jpg";
+
+import { Card } from "../component/Card.js";
 import "../../styles/home.css";
 
 export const Home = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			<div className="alert alert-info">
-				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
+		<div className="text-center mt-5 container">
+			<div class="row">
+				<div class="col-9">
+					<Card/>
+				</div>
 			</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://start.4geeksacademy.com/starters/react-flask">
-					Read documentation
-				</a>
-			</p>
 		</div>
 	);
 };
