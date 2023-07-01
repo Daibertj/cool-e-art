29b7f25273867ee7bf13aae843359177e5758154
Edgar Diaz <ed35376@gmail.com>
2023-06-30 22:51:02 -0400

    useNavigate, sweetalert

--- src/front/js/component/Register.jsx
+++ src/front/js/component/Register.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useContext } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { Context } from "../store/appContext";
+import Swal from 'sweetalert2'
+
 
 const initialState = {
   name: "",
@@ -12,9 +14,18 @@ const initialState = {
 const Register = () => {
   const [user, setUser] = useState(initialState);
   const { actions } = useContext(Context);
+  const navigate = useNavigate()
+  const Swal = require('sweetalert2')
+
   const handleSignup = async () => {
     if (!user.name || !user.email || !user.password) {
       console.log("Por favor completa todos los campos");
+      Swal.fire({
+        title: 'Error!',
+        text: 'Por favor completa todos los campos',
+        icon: 'error',
+        confirmButtonText: 'OK'
+      })
       return;
     }
 
@@ -22,8 +33,17 @@ const Register = () => {
       const response = await actions.registerUser(user);
       if (response === 200) {
         console.log("Registro exitoso");
+        navigate("/")
       } else {
+        Swal.fire({
+          title: 'Error!',
+          text: 'Error en el registro',
+          icon: 'error',
+          confirmButtonText: 'OK'
+        })
+
         console.log("Error en el registro");
+        
       }
     } catch (error) {
       console.log("Error en la solicitud de registro:", error);
@@ -36,7 +56,7 @@ const Register = () => {
 
   return (
     <div className="container-fluid  w-25  ">
-      <h1>Registe</h1>
+      <h1>Register</h1>
       <form>
         <div className="form-group  ">
           <label for="name">Name:</label>
