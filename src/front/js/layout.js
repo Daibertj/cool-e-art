7c4181eb0ad2bfab585e4da888d3adcfa314edff
Edgar Diaz <ed35376@gmail.com>
2023-07-02 11:57:50 -0400

    usrpage created

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -10,6 +10,7 @@ import injectContext from "./store/appContext";
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
 import Register from "./component/Register.jsx";
+import UserPage from "./component/UserPage.jsx";
 
 //create your first component
 const Layout = () => {
@@ -27,6 +28,7 @@ const Layout = () => {
                     <Routes>
                         <Route element={<Home />} path="/" />
                         <Route element={<Register/>} path="/register"/>
+                        <Route element={<UserPage/>} path="/userpage"/> 
                         <Route element={<h1>Not found!</h1>} />
                     </Routes>
                     <Footer />
