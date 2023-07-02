5bd7906fdbd2019658a43d995148e69436a1479d
Edgar Diaz <127860772+edgardiaz1212@users.noreply.github.com>
2023-07-02 16:20:26 +0000

    fix classname in card

--- src/front/js/component/Card.js
+++ src/front/js/component/Card.js
@@ -3,11 +3,11 @@ import React from "react";
 export const Card = () => {
 	return (
 		
-            <div class="card" style={{width: "18rem"}} >
+            <div className="card" style={{width: "18rem"}} >
                 <img src="https://st2.depositphotos.com/23395854/44166/v/450/depositphotos_441666184-stock-illustration-fried-traditional-pastry-stuffed-with.jpg" class="card-img-top" alt="..."/>
-                <div class="card-body">
-                    <h5 class="card-title">Dibujo de empanada</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <div className="card-body">
+                    <h5 className="card-title">Dibujo de empanada</h5>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                     <h5>@Daibert</h5>
                 </div>
             </div>
