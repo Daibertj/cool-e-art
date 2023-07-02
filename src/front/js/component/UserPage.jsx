a774f5c0707062bd09b12732d345af629076264d
Edgar Diaz <ed35376@gmail.com>
2023-07-02 12:45:39 -0400

    basic userpage

--- src/front/js/component/UserPage.jsx
+++ src/front/js/component/UserPage.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
-
+import { Link, useNavigate } from "react-router-dom";
 function UserPage() {
   return (
-    <div>UserPage</div>
+    <div>
+<div className="col-lg-6 col-md-8 mx-auto">
+        <h1 className="fw-light">Album example</h1>
+        <p className="lead text-body-secondary">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don’t simply skip over it entirely.</p>
+        <p>
+          <button className="btn btn-primary my-2">Main call to action</button>
+          <button className="btn btn-secondary my-2">Secondary action</button>
+        </p>
+      </div>
+
+
+
+    </div>
   )
 }
 
