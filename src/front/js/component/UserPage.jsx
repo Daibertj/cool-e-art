4a17480d1cc0b6d244d95c458918e4510e85dc84
Edgar Diaz <ed35376@gmail.com>
2023-07-03 18:08:42 -0400

    userpage

--- src/front/js/component/UserPage.jsx
+++ src/front/js/component/UserPage.jsx
@@ -34,6 +34,7 @@ function UserPage() {
         <div className="album py-5 bg-body-tertiary">
           <div className="container">
             <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+              
               <div>
               <Card/>
               <div className="btn-group">
