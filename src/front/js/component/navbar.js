337a6dd339a3c7fd7a61c41d0860b044e7f3e054
Edgar Diaz <ed35376@gmail.com>
2023-07-03 23:00:49 +0000

    class classname

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -17,28 +17,28 @@ export const Navbar = () => {
 					</Link>
 					</div> */}
 
-				<div class="dropdown">
-          <button type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false" data-bs-auto-close="outside">
+				<div className="dropdown">
+          <button type="button" className="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false" data-bs-auto-close="outside">
             Login
           </button>
-        <form class="dropdown-menu p-4">
-        <div class="mb-3">
-          <label for="exampleDropdownFormEmail2" class="form-label">Email address</label>
-          <input type="email" class="form-control" id="exampleDropdownFormEmail2" placeholder="email@example.com"/>
+        <form className="dropdown-menu p-4">
+        <div className="mb-3">
+          <label htmlFor="exampleDropdownFormEmail2" className="form-label">Email address</label>
+          <input type="email" className="form-control" id="exampleDropdownFormEmail2" placeholder="email@example.com"/>
         </div>
-        <div class="mb-3">
-          <label for="exampleDropdownFormPassword2" class="form-label">Password</label>
-          <input type="password" class="form-control" id="exampleDropdownFormPassword2" placeholder="Password"/>
+        <div className="mb-3">
+          <label htmlFor="exampleDropdownFormPassword2" className="form-label">Password</label>
+          <input type="password" className="form-control" id="exampleDropdownFormPassword2" placeholder="Password"/>
         </div>
-        <div class="mb-3">
-          <div class="form-check">
-            <input type="checkbox" class="form-check-input" id="dropdownCheck2"/>
-            <label class="form-check-label" for="dropdownCheck2">
+        <div className="mb-3">
+          <div className="form-check">
+            <input type="checkbox" className="form-check-input" id="dropdownCheck2"/>
+            <label className="form-check-label" htmlFor="dropdownCheck2">
               Remember me
             </label>
           </div>
         </div>
-          <button type="submit" class="btn btn-primary">Sign in</button>
+          <button type="submit" className="btn btn-primary">Sign in</button>
         </form>
       </div>
 			</div>
