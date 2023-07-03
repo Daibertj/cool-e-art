a9619192b15572af6abdf65d90f149d563e2216c
Edgar Diaz <ed35376@gmail.com>
2023-07-02 20:38:52 -0400

    flux user get

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,60 +1,73 @@
 const getState = ({ getStore, getActions, setStore }) => {
-	return {
-		store: {
-			token: localStorage.getItem("token") || null,
-			message: null,
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
-		},
-		actions: {
-			registerUser: async (user) => {
-				
-				const store = getStore()
-				try {
-				  let response = await fetch(`${process.env.BACKEND_URL}/user`, {
-					method: "POST",
-		
-					body: user
-				  })
-		
-				  let data = await response.json()
-		
-		
-				} catch (error) {
-				  return response.status
-				}
-			  },
+  return {
+    store: {
+      token: localStorage.getItem("token") || null,
+      message: null,
+      demo: [
+        {
+          title: "FIRST",
+          background: "white",
+          initial: "white",
+        },
+        {
+          title: "SECOND",
+          background: "white",
+          initial: "white",
+        },
+      ],
+    },
+    actions: {
+      registerUser: async (user) => {
+        const store = getStore();
+        try {
+          let response = await fetch(`${process.env.BACKEND_URL}/user`, {
+            method: "POST",
 
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
+            body: user,
+          });
 
-			getMessage: async () => {
-				try{
-					// fetching data from the backend
-					const resp = await fetch(process.env.BACKEND_URL + "/api/hello")
-					const data = await resp.json()
-					setStore({ message: data.message })
-					// don't forget to return something, that is how the async resolves
-					return data;
-				}catch(error){
-					console.log("Error loading message from backend", error)
-				}
-			},
-			
-		}
-	};
+          let data = await response.json();
+          return response.status;
+        } catch (error) {
+          return response.status;
+        }
+      },
+
+      // Use getActions to call a function within a fuction
+      exampleFunction: () => {
+        getActions().changeColor(0, "green");
+      },
+
+      UserData: async () => {
+        const store = getStore();
+        try {
+          const response = await fetch(`${process.env.BACKEND_URL}/user`, {
+            method: "GET",
+            headers: {
+              Authorization: `Bearer ${store.token}`,
+            },
+          });
+
+          if (response.ok) {
+            const data = await response.json();
+            console.log("User data:", data);
+            // Actualizar el estado de la aplicación con los datos del usuario obtenidos
+            setStore({ userData: data });
+          } else {
+            
+            console.log("Error fetching user data:", response.status);
+            
+          }
+        } catch (error) {
+          
+          console.log("Error fetching user data:", error);
+          
+        }
+      },
+
+      },
+    },
+  };
 };
 
 export default getState;
