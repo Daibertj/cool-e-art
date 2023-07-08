76952331dd56cffaa6282a82b74ecfc1f10687b2
Edgar Diaz <ed35376@gmail.com>
2023-07-07 20:57:21 -0400

    profile favorites step 1

--- src/front/js/component/UserPage.jsx
+++ src/front/js/component/UserPage.jsx
@@ -28,9 +28,9 @@ function UserPage() {
             <h1 className="fw-light">{userData.name}</h1>
           </div>
         </div>
-        <div className="d-flex align-items-center p-3 my-3 border border-success rounded shadow-sm">
+        <div className="d-flex align-items-center p-3 my-3 rounded shadow-sm text-white barra">
           <div className="lh-1">
-            <h1 className="mb-0 text-blue lh-1">Mis creaciones</h1>
+            <h2 className="mb-0 lh-1">Mis creaciones</h2>
           </div>
         </div>
 
@@ -65,7 +65,41 @@ function UserPage() {
             </div>
           </div>
         </div>
-        <p className="container">Mis Favoritos</p>
+        <div className="d-flex align-items-center p-3 my-3 rounded shadow-sm text-white barra">
+          <div className="lh-1">
+            <h2 className="mb-0 lh-1">Favoritos</h2>
+          </div>
+          </div>
+          <div className="album py-5 bg-body-tertiary">
+          <div className="container">
+            <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+              
+              {/* cambiar la logica de este map*/}
+              {store.ilustrationData.map((ilustration) => (
+                <div className="col" key={ilustration.id}>
+                  <Card
+                    image={ilustration.image}
+                    title={ilustration.title}
+                    description={ilustration.description}
+                    user={ilustration.user}
+                  />
+
+                  <div className="btn-group">
+                    <button
+                      type="button"
+                      className="btn btn-sm btn-outline-secondary"
+                    >
+                      View
+                    </button>
+                    
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+
+        
       </div>
     </>
   );
