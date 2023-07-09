f57295179569d0d985e9e3632c1229236bd5ebba
Edgar Diaz <ed35376@gmail.com>
2023-07-09 19:54:02 -0400

    key fixed

--- src/front/js/component/UserPage.jsx
+++ src/front/js/component/UserPage.jsx
@@ -6,7 +6,7 @@ import { Card } from "./Card";
 
 function UserPage() {
   const { actions, store } = useContext(Context);
-  const { userData, ilustrationData } = store;
+  const { ilustrationData } = store;
   const { getUserData } = actions;
 
   const { alias } = useParams();
@@ -15,16 +15,15 @@ function UserPage() {
     getUserData(alias);
   }, [alias]);
 
-  const userIlustrations = store.ilustrationData.filter(
+  const userIlustrations = ilustrationData.filter(
     (ilustration) => ilustration.user.alias === alias
   );
   const userProfile =
     userIlustrations.length > 0 ? userIlustrations[0].user : null;
-
-  console.log(userIlustrations);
+  
   return (
     <>
-      <div className="border border-danger">
+      <div >
         <div className="container d-inline-flex justify-content-center border border-danger ">
           <img
             src={userProfile.image}
@@ -33,7 +32,7 @@ function UserPage() {
             style={{ width: "150px" }}
           />
           <div className="col-lg-6 col-md-8 h-25  ">
-            <h1 className="fw-light">{userProfile ? userProfile.alias : ""}</h1>
+            <h1 className="fw-light">{userProfile.alias}</h1>
             <p className="fst-italic">
               {userProfile.name} {userProfile.lastname}
             </p>
@@ -50,7 +49,7 @@ function UserPage() {
           <div className="container">
             <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
               {userIlustrations.map((ilustration) => (
-                <div className="col" key={ilustration.user.id}>
+                <div className="col" key={ilustration.id}>
                   <Card
                     image={ilustration.image}
                     title={ilustration.title}
