1efeafb977f17ad2da995fc3180b7ff08ed555d8
Edgar Diaz <ed35376@gmail.com>
2023-07-09 21:25:10 -0400

    detalles en view

--- src/front/js/component/ImageView.jsx
+++ src/front/js/component/ImageView.jsx
@@ -6,14 +6,16 @@ function ImageView() {
  const {id}=useParams()
  const {store} =useContext(Context)
 const {ilustrationData} = store
-console.log(id)
 const ilustration = ilustrationData.find((ilustration)=>ilustration.id === parseInt(id))
+
 return (
     <div className="text-center">
         <img 
         src={ilustration.image} 
         alt={ilustration.title}
         />
+        <h1>{ilustration.title}</h1>
+        <p>{ilustration.description}</p>
 
     </div>
   )
