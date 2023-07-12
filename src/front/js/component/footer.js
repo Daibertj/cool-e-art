f8fd57902db7907ad45481989e14b0423af5514e
Edgar Diaz <ed35376@gmail.com>
2023-07-12 19:07:20 -0400

    logo

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -3,6 +3,6 @@ import coolLogo from "../../img/logo_no_back.png";
 
 export const Footer = () => (
 	<footer className="footer mt-auto py-3 text-center">
-		<img className= "logo1"  src={coolLogo} />
+		<img className= "logo"  src={coolLogo} />
 	</footer>
 );
