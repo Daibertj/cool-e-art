8423beef5b05c3832941ec87ff0afc9c2069bfe7
Daibert Ramirez <126725296+Daibertj@users.noreply.github.com>
2023-07-13 15:59:41 +0000

    fix cars

--- src/front/js/component/Card.js
+++ src/front/js/component/Card.js
@@ -6,28 +6,30 @@ import { Context } from "../store/appContext";
 export const Card = ({ id, title, description, image, user }) => {
     const { store, actions } = useContext(Context)
     return (
-<>
-        <div >
-            <div className="card">
-                <Link to={`/imageview/${id}`}>
-                    <img
-                        src={image}
-                        className="card-img-top"
-                        alt={title}
-                        style={{ width: "100%", height: "auto" }}
-                    />
-                </Link>
-                <div className="card-body">
-                    <h5 className="card-title">{title}</h5>
-                    <p className="card-text">{description ? description : "Sin descripcion"}</p>
-                    <Link to={`/profile/${user.alias}`} style={{ textDecoration: 'none', color: 'grey' }}><p>{user.name}, aca</p> </Link>
+        <>
+            <div className="col-12 col-md-3 col-sm-4">
+                <div className="card">
+                    <Link to={`/imageview/${id}`}>
+                        <img
+                            src={image}
+                            className="card-img-top"
+                            alt={title}
+                            style={{ width: "100%", height: "auto" }}
+                        />
+                    </Link>
+                    <div className="card-body">
+                        <h5 className="card-title">{title}</h5>
+                        <p className="card-text">{description ? description : "Sin descripcion"}</p>
+                        <Link to={`/profile/${user.alias}`} style={{ textDecoration: 'none', color: 'grey' }}><p>{user.name}</p> </Link>
+                    </div>
                 </div>
+                <button
+                    className="btn btn-dark bg-black fa-solid fa-heart"
+                    onClick={() => actions.addFavorite(id)}
+                ></button>
             </div>
-            <button
-                className="btn btn-dark bg-black fa-solid fa-heart"
-                onClick={() => actions.addFavorite(id)}
-            ></button>
-        </div>
-</>
+        </>
     );
 };
+
+
