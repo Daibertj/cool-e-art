d3f28568c67d912b0d37741550198a6598ace2c8
Daibert Ramirez <126725296+Daibertj@users.noreply.github.com>
2023-07-13 15:25:29 +0000

    presentacion preliminar

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -41,135 +41,135 @@ export const Navbar = () => {
     });
   };
   return (
- 
-<nav className="navbar navbar-expand-lg navbar-secondary  ">
-  <div className="container text-white">
-    <Link className="navbar-brand text-white bold-text text-capitalize fs-4 fw-bold fst-italic" to="#">Cool-e-Art</Link>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
 
-    <div className="collapse navbar-collapse text-white" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        {!store.token && (
-          <>
-            <li className="nav-item text-white">
-              <Link to="/" className="nav-link text-white" aria-current="page">Home</Link>
-            </li>
-          </>
-        )}
+    <nav className="navbar navbar-expand-lg navbar-secondary  ">
+      <div className="container text-white">
+        <Link className="navbar-brand text-white bold-text text-capitalize fs-3 fw-bold fst-italic" to="/">Cool-e-Art</Link>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
 
-        {store.token && (
-          <>
-            <li className="nav-item">
-              <Link to="/" className="nav-link text-white" aria-current="page">Home</Link>
-            </li>
-            <li className="nav-item">
-              <Link to={`/profile/${store.userData.alias}`} className="nav-link text-white">My Profile</Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/upload" className="nav-link text-white">Upload Creation</Link>
-            </li>
-          </>
-        )}
-      </ul>
+        <div className="collapse navbar-collapse text-white" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            {/* {!store.token && (
+              <>
+                <li className="nav-item text-white">
+                  <Link to="/" className="nav-link text-white" aria-current="page">Home</Link>
+                </li>
+              </>
+            )} */}
 
-      <div className="ms-auto d-flex">
-        {!store.token && (
-          <>
-            <div className="me-2">
-              <Link to="/register" className="btn btn-secondary">Registro</Link>
-            </div>
-            <div className="dropdown">
-              <button
-                type="button"
-                className="btn btn-secondary dropdown-toggle"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-                data-bs-auto-close="outside"
-              >
-                Login
-              </button>
-              <form className="dropdown-menu p-4">
-              <div className="mb-3">
-                  <label
-                    htmlFor="exampleDropdownFormEmail2"
-                    className="form-label"
-                  >
-                    Email address
-                  </label>
+            {store.token && (
+              <>
+                {/* <li className="nav-item">
+                  <Link to="/" className="nav-link text-white" aria-current="page">Home</Link>
+                </li> */}
+                <li className="nav-item">
+                  <Link to={`/profile/${store.userData.alias}`} className="nav-link text-white">My Profile</Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/upload" className="nav-link text-white">Upload Creation</Link>
+                </li>
+              </>
+            )}
+          </ul>
 
-                  <input
-                    onChange={handleChange}
-                    name="email"
-                    type="email"
-                    className="form-control"
-                    id="exampleDropdownFormEmail2"
-                    placeholder="email@example.com"
-                  />
+          <div className="ms-auto d-flex">
+            {!store.token && (
+              <>
+                <div className="me-2">
+                  <Link to="/register" className="btn btn-secondary">Registro</Link>
                 </div>
-                <div className="mb-3">
-                  <label
-                    htmlFor="exampleDropdownFormPassword2"
-                    className="form-label"
+                <div className="dropdown">
+                  <button
+                    type="button"
+                    className="btn btn-secondary dropdown-toggle"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                    data-bs-auto-close="outside"
                   >
-                    Password
-                  </label>
-                  <input
-                    onChange={handleChange}
-                    name="password"
-                    type="password"
-                    className="form-control"
-                    id="exampleDropdownFormPassword2"
-                    placeholder="Password"
-                  />
-                </div>
-                <div className="mb-3">
-                  <div className="form-check">
-                    <input
-                      type="checkbox"
-                      className="form-check-input"
-                      id="dropdownCheck2"
-                    />
-                    <label
-                      className="form-check-label"
-                      htmlFor="dropdownCheck2"
+                    Login
+                  </button>
+                  <form className="w-100 border border-danger dropdown-menu dropdown-menu-end p-4">
+                    <div className="mb-3" >
+                      <label
+                        htmlFor="exampleDropdownFormEmail2"
+                        className="form-label"
+                      >
+                        Email address
+                      </label>
+
+                      <input
+                        onChange={handleChange}
+                        name="email"
+                        type="email"
+                        className="form-control"
+                        id="exampleDropdownFormEmail2"
+                        placeholder="email@example.com"
+                      />
+                    </div>
+                    <div className="mb-3">
+                      <label
+                        htmlFor="exampleDropdownFormPassword2"
+                        className="form-label"
+                      >
+                        Password
+                      </label>
+                      <input
+                        onChange={handleChange}
+                        name="password"
+                        type="password"
+                        className="form-control"
+                        id="exampleDropdownFormPassword2"
+                        placeholder="Password"
+                      />
+                    </div>
+                    <div className="mb-3">
+                      <div className="form-check">
+                        <input
+                          type="checkbox"
+                          className="form-check-input"
+                          id="dropdownCheck2"
+                        />
+                        <label
+                          className="form-check-label"
+                          htmlFor="dropdownCheck2"
+                        >
+                          Remember me
+                        </label>
+                      </div>
+                    </div>
+                    <button
+                      type="button"
+                      className="loginButton btn btn-secondary w-100 mt-3"
+                      onClick={() => handleLogin()}
                     >
-                      Remember me
-                    </label>
-                  </div>
+                      Sign in
+                    </button>
+                  </form>
                 </div>
-                <button
-                  type="button"
-                  className="loginButton btn btn-secondary w-100 mt-3"
-                  onClick={() => handleLogin()}
-                >
-                  Sign in
-                </button>
-              </form>
-            </div>
-          </>
-        )}
+              </>
+            )}
 
-        {store.token && (
-          <>
-          
-            <span className="m-2">Hey, {userData.alias}!</span>
-          
-          
-      <button
-        className=" btn btn-secondary "
-        onClick={()=> actions.logout()}
-      >
-        Log Out
-      </button>
-    
-          </>
-        )}
+            {store.token && (
+              <>
+
+                <span className="m-2">Hey, {userData.alias}!</span>
+
+
+                <button
+                  className=" btn btn-secondary "
+                  onClick={() => actions.logout()}
+                >
+                  Log Out
+                </button>
+
+              </>
+            )}
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-</nav>
+    </nav>
 
 
   );
