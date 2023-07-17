27f8d13bf1c6f9e867869b8e32a06f840776afa4
Victor Pantin <125493189+tuquepantin@users.noreply.github.com>
2023-07-17 22:29:34 +0000

    cambios card

--- src/front/js/component/Edit.jsx
+++ src/front/js/component/Edit.jsx
@@ -1,30 +1,28 @@
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { Context } from "../store/appContext";
-
+import "../../styles/home.css";
 
 export const Edit = ({ id, title, description, image, user }) => {
     const { store, actions } = useContext(Context)
     return (
         <>
             <div>
-                <div class="card mb-3" style={{ width: "540px"}}>
+                <div class="card mb-3" style={{ width: "700px", height: "250px" }}>
                     <div class="row g-0">
                         <div class="col-md-4">
-                        <Link to={`/imageview/${id}`}>
-                        <img
-                            src={image}
-                            className="card-img-top"
-                            alt={title}
-                            style={{ width: "100%", height: "auto" }}
-                        />
-                    </Link>
+                            <img
+                                src={image}
+                                className="card-img-top"
+                                alt={title}
+                                style={{ width: "200px", height: "250px" }}
+                            />
                         </div>
                         <div class="col-md-8">
                             <div class="card-body">
-                            <h5 className="card-title">{title}</h5>
-                        <p className="card-text">{description ? description : "Sin descripcion"}</p>
-                        <Link to={`/profile/${user.alias}`} style={{ textDecoration: 'none', color: 'grey' }}><p>{user.name}</p> </Link>
+                                <h5 className="card-title">{title}</h5>
+                                <p className="card-text">{description ? description : "Sin descripcion"}</p>
+                                <Link to={`/profile/${user.alias}`} style={{ textDecoration: 'none', color: 'grey' }}><p>{user.name}</p> </Link>
                             </div>
                         </div>
                     </div>
