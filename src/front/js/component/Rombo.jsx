c4ad7b8ae19caacce45e3c8191f00efc85adf41f
Victor Pantin <125493189+tuquepantin@users.noreply.github.com>
2023-07-19 22:50:36 +0000

    cambios rombo

---
+++ src/front/js/component/Rombo.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import coolLogo from "../../img/logo_no_back.png";
+import { Link } from "react-router-dom";
+
+
+export const Rombo = () => (
+    <div className="wrapper">
+        <img className="imgrombo border border-danger" src="https://source.unsplash.com/random/600x600?water" alt="" />
+        <img className="imgrombo" src="https://source.unsplash.com/random/600x600?summer" alt="" />
+        <img className="imgrombo" src="https://source.unsplash.com/random/600x600?plants" alt="" />
+        <img className="imgrombo" src="https://source.unsplash.com/random/600x600?snow" alt="" />
+        <img className="imgrombo" src="https://source.unsplash.com/random/600x600?roses" alt="" />
+        <img className="imgrombo" src="https://source.unsplash.com/random/600x600?sky" alt="" />
+        <img className="imgrombo" src="https://source.unsplash.com/random/600x600?nature" alt="" />
+        <img className="imgrombo" src="https://source.unsplash.com/random/600x600?blossom" alt="" />
+        <img className="imgrombo" src="https://source.unsplash.com/random/600x600?ice" alt="" />
+        <img className="imgrombo" src="https://source.unsplash.com/random/600x600?spring" alt="" />
+    </div>
+);
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -3,28 +3,30 @@ import { Context } from "../store/appContext";
 import rigoImageUrl from "../../img/rigo-baby.jpg";
 
 import { Card } from "../component/Card.js";
-import "../../styles/home.css";
+import { Rombo } from "../component/Rombo.jsx";
+import "../../styles/rombo.css";
 
 export const Home = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
-		<div className="text-center mt-5 container">
+    <Rombo />
+		// <div className="text-center mt-5 container">
 			
-      <div className="row">
-          {store.photos.map((photo) => 
-             (
-              <Card
-                key={photo.id}
-                title={photo.photographer}
-                description={photo.photographer}
-                image={photo.src.medium}
-                user={photo.photographer}
-                id={photo.id}
-              />
+    //   <div className="row">
+    //       {store.photos.map((photo) => 
+    //          (
+    //           <Card
+    //             key={photo.id}
+    //             title={photo.photographer}
+    //             description={photo.photographer}
+    //             image={photo.src.medium}
+    //             user={photo.photographer}
+    //             id={photo.id}
+    //           />
               
-            ))}
-        </div>
-		</div>
+    //         ))}
+    //     </div>
+		// </div>
 	);
 };
