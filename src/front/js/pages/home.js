d03c945207d93bc72c02877a123d31f39eb79529
Victor Pantin <125493189+tuquepantin@users.noreply.github.com>
2023-07-21 17:20:33 +0000

    corregir home

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -7,26 +7,21 @@ import { Rombo } from "../component/Rombo.jsx";
 import "../../styles/rombo.css";
 
 export const Home = () => {
-	const { store, actions } = useContext(Context);
+  const { store, actions } = useContext(Context);
 
-	return (
-    <Rombo />
-		// <div className="text-center mt-5 container">
-			
-    //   <div className="row">
-    //       {store.photos.map((photo) => 
-    //          (
-    //           <Card
-    //             key={photo.id}
-    //             title={photo.photographer}
-    //             description={photo.photographer}
-    //             image={photo.src.medium}
-    //             user={photo.photographer}
-    //             id={photo.id}
-    //           />
-              
-    //         ))}
-    //     </div>
-		// </div>
-	);
+  return (
+    <div>
+      <div className="wrapper">
+      {store.photos.map((photo) =>
+      (
+        <Rombo
+          key={photo.id}
+          image={photo.src.medium}
+        />
+      ))}
+    </div>
+    </div>
+
+    
+  );
 };
