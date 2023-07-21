624af0eed12fff661dfb0b0b7a9e5d6bb75fe228
Edgar Diaz <ed35376@gmail.com>
2023-07-20 21:01:51 -0400

    profile images github

--- src/front/js/pages/AboutUs.jsx
+++ src/front/js/pages/AboutUs.jsx
@@ -1,48 +1,44 @@
 import React from 'react'
-import coolLogo from "../../img/logo_no_back.png";
+import daibert from "../../img/Daibert.jpg"
+import victor from "../../img/Victor.jpg"
+import edgar from '../../img/Edgar.png'
+import fondo from "../../img/pic04.jpg"
 import { Link } from 'react-router-dom';
 
 
 function AboutUs() {
+    
+
     return (
         <>
-
+     <div className="container text-center">
             <div id="banner">
-                <h1>About Us</h1>
+                <h1 className="display-4 mb-4">Bienvenidos a Cool-E-Art</h1>
+                <p className="lead fs-5">
+                    Somos un grupo de nuevos desarrolladores con ganas de aportar nuestro grano de arena en este mundo del arte del dibujo. Por eso hemos ideado una página donde los artistas pueden mostrar su arte. El momento es ahora de crear e imaginar las diversas posibilidades.
+                </p>
             </div>
-            <div className='container d-flex border border-danger ' >
-                <img className="logo1 border border-danger " src={coolLogo} />
+           
+            
+            <div className='my-5 '>
+            <h2 className="display-5">Miembros</h2>
+            <div className='row justify-content-center'>
 
-                <div>
-                    <p className="text-break   border border-danger">
-                        ¿Pensabas que el arte había muerto?
-                        De hecho el arte en todas sus facetas ha trascendido toda clase de desgracias humanas, entre ellas guerras, pandemias y muchas otras, siendo un medio de catarsis y desahogo para las malas situaciones vividas por la humanidad.
-                        En esta era tecnológica que cada vez avanza más rápido, aunque a veces ni nos damos cuenta, llegó una alternativa para los artistas gráficos (léase pintores, dibujantes,
-                        diseñadores, fotógrafos, etc...) para montarse en la ola tecnológica y trascender a través de su arte en el tiempo, con nuestra página web Cool-E-Art; exclusivamente diseñada para llevar a todos los rincones del mundo esas obras que tenías guardando polvo.
-                        Artistas, expertos y aficionados son bienvenidos a nuestra comunidad ¿Qué esperas? ¡Ven a ver!
-                    </p>
-
-                </div>
-            </div>
-            <div className='space'>
-                <br />
-
-            </div>
-            <div className='container '>
-                <h2>Miembros</h2>
-                <div className='row'>
-
-                    <h4><Link to="https://github.com/tuquepantin">imagen</Link>  Victor Pantin</h4>
-                    <h4>Daibert Ramirez <Link to="https://github.com/Daibertj">iamgen</Link></h4>
-                    <h4><Link to="https://github.com/edgardiaz1212">imagen</Link> Edgar Diaz</h4>
+                    <h4><Link to="https://github.com/tuquepantin"><img className="perfiles rounded-circle " src={victor} /></Link>  Victor Pantin</h4>
+                    <h4>Daibert Ramirez <Link to="https://github.com/Daibertj"><img className="perfiles rounded-circle  " src={daibert} /></Link></h4>
+                    <h4><Link to="https://github.com/edgardiaz1212"><img className="perfiles rounded-circle  " src={edgar} /></Link> Edgar Diaz</h4>
                 </div>
             </div>
 
             <div className='container p-4'>
-            <h2>Agradecimientos</h2>
-            
-            <Link to="https://4geeksacademy.com" className='text-center'>4GeeksAcademy</Link>
+                <h2>Agradecimientos</h2>
+<h4><Link to="https://4geeksacademy.com" className='text-center'>4GeeksAcademy</Link></h4>
+                
             </div>
+</div>
+            
+
+
 
         </>
     )
