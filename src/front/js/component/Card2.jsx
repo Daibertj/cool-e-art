5a6b6e0e8653245c1f21f01f2f9ccd9ff0867147
Daibert Ramirez <126725296+Daibertj@users.noreply.github.com>
2023-07-22 21:40:42 +0000

    corazon n la card

--- src/front/js/component/Card2.jsx
+++ src/front/js/component/Card2.jsx
@@ -17,15 +17,16 @@ function Card2({ id, title, description, image, alias }) {
           />
         </Link>
         <div className="card-body">
-          <h5 className="card-title text-white">{title}</h5>
+          <h5 className="card-title text-black">{title}</h5>
           <p className="card-text">{description ? description : "Sin descripcion"}</p>
           <Link to={`/profile/${alias}`} style={{ textDecoration: 'none', color: 'grey' }}><p>{alias}</p> </Link>
         </div>
-      </div>
-      <button
+        <button
         className={store.favoriteData.some(favorite => favorite.ilustration_id == id) ? "btn btn-dark bg-black fa-solid fa-heart" : "btn btn-outline-dark bg-black fa-solid fa-heart"}
         onClick={() => actions.addFavorite(id)}
       ></button>
+      </div>
+      
     </>
   )
 }
