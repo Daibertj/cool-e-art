4bc79680e1a921693d55b8092b7dd6c6eea1995e
Edgar Diaz <ed35376@gmail.com>
2023-07-27 19:42:29 -0400

    toast done

--- src/front/js/pages/UploadImage.jsx
+++ src/front/js/pages/UploadImage.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useContext } from "react";
 import { Context } from "../store/appContext";
 import { ToastContainer, toast } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
 
 const initialState = {
   image: "",
@@ -16,17 +16,6 @@ const UploadImage = () => {
   const { categories } = store;
 
   const handleUpload = async () => {
-    if (
-      !imgUpload.image ||
-      !imgUpload.description ||
-      !imgUpload.title ||
-      !imgUpload.category
-    ) {
-      toast.error("Please fill all fields");
-      console.log("missing parameter");
-      return;
-    }
-
     try {
       const formData = new FormData();
 
@@ -38,16 +27,17 @@ const UploadImage = () => {
       const response = await actions.uploadIlustration(formData);
 
       if (response.status === 201 || response.status === 200) {
-        toast.success("Image Uploaded", { theme: "dark" });
         console.log("Image Uploaded:", {
           imgUpload,
         });
+        return true;
       } else {
-        toast.error("Upload failed", { theme: "dark" });
         console.log("Error en Upload");
+        return false; 
       }
     } catch (error) {
       console.log("Error en la solicitud de Upload:", error);
+      return false; 
     }
   };
 
@@ -55,17 +45,29 @@ const UploadImage = () => {
     setImgUpload({ ...imgUpload, [target.name]: target.value });
   };
 
-  const handleUploadPromise = () => {
-    return new Promise((resolve, reject) => {
-      handleUpload()
-        .then(() => resolve())
-        .catch((error) => reject(error));
+  const handleUploadNotification = async () => {
+    if (
+      !imgUpload.image ||
+      !imgUpload.description ||
+      !imgUpload.title ||
+      !imgUpload.category
+    ) {
+      toast.error("Please fill all fields")
+      console.log("missing parameter");
+      return;
+    }
+//toma handelupload como retorno de promesa 
+    toast.promise(handleUpload(), {
+      pending: "Uploading...",
+      success: "Uploaded",
+      error: "Upload failed",
+      
     });
   };
 
   return (
     <>
-    <ToastContainer theme="dark"/>
+    <ToastContainer theme="dark" position= "top-center" pauseOnFocusLoss={false} autoClose={3000}/>
     <div className="container-fluid text-white my-5 pt-5 w-25 vh-100">
       <h1>Upload</h1>
       <form>
@@ -118,14 +120,7 @@ const UploadImage = () => {
       </form>
       <button
   className="btn btn-secondary w-100 mt-3"
-  onClick={() =>
-    toast.promise(handleUploadPromise(), {
-      pending: "Uploading...",
-      success: "Uploaded",
-      error: "Upload failed",
-      theme: "dark",
-    })
-  }
+  onClick={handleUploadNotification}
 >Upload</button>
     </div>
 </>
