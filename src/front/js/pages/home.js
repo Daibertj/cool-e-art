2f5d03d9a203ac9d73f5b213a2e57742629acd67
Edgar Diaz <ed35376@gmail.com>
2023-07-29 11:33:53 -0400

    landing donde, new logout

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -6,6 +6,13 @@ import { Card } from "../component/Card.js";
 import { Rombo } from "../component/Rombo.jsx";
 import "../../styles/rombo.css"
 
+import daibert from "../../img/Daibert.jpg"
+import victor from "../../img/Victor.jpg"
+import edgar from '../../img/Edgar.png'
+import { Link } from 'react-router-dom'
+import geeks from '../../img/geeks.png'
+
+
 export const Home = () => {
   const { store, actions } = useContext(Context);
 
@@ -52,75 +59,102 @@ export const Home = () => {
             </h2>
             <p>El lugar de ilustradores para ilustradores</p>
           </header>
-          <span className="image featured"><img src="images/pic01.jpg" alt="" /></span>
+          
         </section>
 
-       <div className="row m-3">
-        
-       <div className=" col card-container p-3">
-          <div className="card-home">
-            <div className="front-content">
-              <p>El arte como forma de catarsis</p>
-            </div>
-            <div className="content">
-              
-              <p>
-              A lo largo de la historia, el arte ha sido un medio de catarsis y desahogo para la humanidad, trascendiendo desgracias humanas como guerras y pandemias. Ha sido una manera de expresar emociones y superar situaciones difíciles.
-              </p>
+        <div className="row m-3">
+
+          <div className=" col card-container p-3">
+            <div className="card-home">
+              <div className="front-content">
+                <p>El arte como forma de catarsis</p>
+              </div>
+              <div className="content">
+
+                <p>
+                  A lo largo de la historia, el arte ha sido un medio de catarsis y desahogo para la humanidad, trascendiendo desgracias humanas como guerras y pandemias. Ha sido una manera de expresar emociones y superar situaciones difíciles.
+                </p>
+              </div>
             </div>
           </div>
-        </div>
 
-        
-        <div className="col card-container">
-          <div className="card-home">
-            <div className="front-content">
-              <p>Una comunidad para artistas y aficionados</p>
-            </div>
-            <div className="content">
-              
-              <p>
-              Cool-E-Art es una comunidad acogedora que da la bienvenida a artistas, expertos y aficionados del arte. Todos pueden compartir y apreciar obras increíbles de diversas disciplinas.
-              </p>
+
+          <div className="col card-container">
+            <div className="card-home">
+              <div className="front-content">
+                <p>Una comunidad para artistas y aficionados</p>
+              </div>
+              <div className="content">
+
+                <p>
+                  Cool-E-Art es una comunidad acogedora que da la bienvenida a artistas, expertos y aficionados del arte. Todos pueden compartir y apreciar obras increíbles de diversas disciplinas.
+                </p>
+              </div>
             </div>
           </div>
-        </div>
 
-       </div>
+        </div>
 
         <div className="row m-3">
-<div className="card-container col">
-          <div className="card-home">
-            <div className="front-content">
-              <p>Cool-E-Art</p>
-            </div>
-            <div className="content">
-              
-              <p>
-              En la era tecnológica actual, Cool-E-Art ofrece una alternativa para artistas gráficos como pintores, dibujantes, diseñadores y fotógrafos. Es una página web diseñada exclusivamente para llevar sus obras al mundo y trascender en el tiempo.
-              </p>
+          <div className="card-container col">
+            <div className="card-home">
+              <div className="front-content">
+                <p>Cool-E-Art</p>
+              </div>
+              <div className="content">
+
+                <p>
+                  En la era tecnológica actual, Cool-E-Art ofrece una alternativa para artistas gráficos como pintores, dibujantes, diseñadores y fotógrafos. Es una página web diseñada exclusivamente para llevar sus obras al mundo y trascender en el tiempo.
+                </p>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="card-container col">
-          <div className="card-home">
-            <div className="front-content">
-              <p>Descubre el arte más cool en internet</p>
-            </div>
-            <div className="content">
-              
-              <p>
-              En Cool-E-Art, puedes encontrar el arte más cool de internet. Explora una variedad de obras únicas y creativas que antes podrían haber estado guardadas y ahora pueden llegar a todos los rincones del mundo. No esperes más, ¡únete a esta comunidad artística!
-              </p>
+          <div className="card-container col">
+            <div className="card-home">
+              <div className="front-content">
+                <p>Descubre el arte más cool en internet</p>
+              </div>
+              <div className="content">
+
+                <p>
+                  En Cool-E-Art, puedes encontrar el arte más cool de internet. Explora una variedad de obras únicas y creativas que antes podrían haber estado guardadas y ahora pueden llegar a todos los rincones del mundo. No esperes más, ¡únete a esta comunidad artística!
+                </p>
+              </div>
             </div>
           </div>
-        </div>
 
 
         </div>
 
-        
+
       </section>
+
+      <div className="container-fluid text-center">
+                <div id="banner">
+                    <h1 className="display-4 mb-4">Sobre los creadores de Cool-E-Art</h1>
+                    <p className="lead fs-5">
+                        Somos un grupo de nuevos desarrolladores con ganas de aportar nuestro grano de arena en este mundo del arte del dibujo. Por eso hemos ideado una página donde los artistas pueden mostrar su arte. El momento es ahora de crear e imaginar las diversas posibilidades.
+                    </p>
+                </div>
+
+
+                <div className='my-5 text-white '>
+                    
+                    <div className='row justify-content-center m-5 p-4'>
+
+                        <h4><Link to="https://github.com/tuquepantin"><img className="perfiles rounded-circle " src={victor} /></Link>  Victor Pantin</h4>
+                        <h4>Daibert Ramirez <Link to="https://github.com/Daibertj"><img className="perfiles rounded-circle  " src={daibert} /></Link></h4>
+                        <h4><Link to="https://github.com/edgardiaz1212"><img className="perfiles rounded-circle  " src={edgar} /></Link> Edgar Diaz</h4>
+                    </div>
+                </div>
+
+                <div className='container p-3 text-white'>
+                    <h4>Entrenados por</h4>
+                    <img className="perfiles-geeks rounded-circle p-2 " src={geeks} />
+                    <h5><Link to="https://4geeksacademy.com" className='text-center '>4GeeksAcademy</Link></h5>
+
+                </div>
+            </div>
 
     </>
 
